d284a70ce96c82128f0d94434df67504138c0710
Mihaela Trif <trifmihaela_88@yahoo.com>
2020-06-01 18:11:26 +0300

    Initial Commit

---
+++ firstpy.py
@@ -0,0 +1,47 @@
+#this is the first commit
+
+text = "# this is the first qoute"
+print(text)
+
+#number operations
+
+one = 5
+two = 7
+
+sum = one + two
+print(sum)
+
+difference = two - one
+print(difference)
+
+divide = one / two
+print(divide)
+
+multiple = two * one
+print(multiple)
+
+print(two**one)
+
+#secound strings
+
+one_string = "Hello kitty"
+print(one_string)
+
+secound_string = "This is a beautiful day"
+print(secound_string)
+
+#third lists
+
+squares = [1, 7, 14, 21, 28, 35, 42]
+print(squares[-1])
+print(squares[0])
+
+lists = [1, 8, 16, 24, 32, 40, 48, 56, 72, 80]
+
+# for list in lists:
+
+# if list % 2 == 0:
+
+# print(list)
+
+
